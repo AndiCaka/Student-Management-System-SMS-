04af039acd73f4c0a9396158f98daaeb6dae58a1
AndiCaka <andicaka9@gmail.com>
2022-09-28 10:56:34 +0200

    login change

--- login.php
+++ login.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <a href="css/login.css"></a>
+    <a href="login.css"></a>
 </head>
 <body>
     
