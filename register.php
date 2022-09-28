fc617688eab6b2507d5596cfa136c65a50d08426
AndiCaka <andicaka9@gmail.com>
2022-09-28 16:12:30 +0200

    register upgrade

--- register.php
+++ register.php
@@ -1,24 +1,5 @@
-
 <!DOCTYPE html>
 <html lang="en">
-
-<!-- Mirrored from preschool.dreamguystech.com/php-template/forgot-password.php by HTTrack Website Copier/3.x [XR&CO'2014], Thu, 28 Oct 2021 11:11:58 GMT -->
-<head>
-<meta charset="utf-8">
-<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0">
-<title>Preskool - Forgot Password</title>
-
-<link rel="shortcut icon" href="assets/img/favicon.png">
-<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,500;0,600;0,700;1,400&amp;display=swap">
-
-<link rel="stylesheet" href="assets/plugins/bootstrap/css/bootstrap.min.css">
-
-<link rel="stylesheet" href="assets/plugins/fontawesome/css/fontawesome.min.css">
-<link rel="stylesheet" href="assets/plugins/fontawesome/css/all.min.css">
-
-<link rel="stylesheet" href="assets/css/style.css">
-</head>
-<body>
 
 <?php
 if(isset($_POST['register'])){
@@ -28,8 +9,8 @@ if(isset($_POST['register'])){
     
     if(!($product = mysqli_fetch_assoc($isAccountQuery))){
         $sql = "INSERT into user 
-        (name, surname, age, gender, username, password, role)
-        VALUES ('".$_POST['name']."', '".$_POST['surname']."', '".$_POST['age']."', '".$_POST['gender']."', '".$_POST['username']."' , '".$_POST['password']."','".$_POST['role']."')";
+        (name, surname, gender, username, password, role)
+        VALUES ('".$_POST['name']."', '".$_POST['surname']."', '".$_POST['gender']."', '".$_POST['username']."' , '".$_POST['password']."','".$_POST['role']."')";
 
         $result = mysqli_query($conn, $sql);
         if(!$result){
@@ -53,6 +34,24 @@ if(isset($_POST['register'])){
 }
 ?>
 
+<!-- Mirrored from preschool.dreamguystech.com/php-template/register.php by HTTrack Website Copier/3.x [XR&CO'2014], Thu, 28 Oct 2021 11:11:58 GMT -->
+<head>
+<meta charset="utf-8">
+<meta name="viewport" >
+<title>Preskool - Register</title>
+
+<link rel="shortcut icon" href="assets/img/favicon.png">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,500;0,600;0,700;1,400&amp;display=swap">
+
+<link rel="stylesheet" href="assets/plugins/bootstrap/css/bootstrap.min.css">
+
+<link rel="stylesheet" href="assets/plugins/fontawesome/css/fontawesome.min.css">
+<link rel="stylesheet" href="assets/plugins/fontawesome/css/all.min.css">
+
+<link rel="stylesheet" href="assets/css/style.css">
+</head>
+<body>
+
 <div class="main-wrapper login-body">
 <div class="login-wrapper">
 <div class="container">
@@ -62,38 +61,44 @@ if(isset($_POST['register'])){
 </div>
 <div class="login-right">
 <div class="login-right-wrap">
-<h1>Forgot Password?</h1>
-<p class="account-subtitle">Enter your email to get a password reset link</p>
+<h1>Register</h1>
+<p class="account-subtitle">Access to our dashboard</p>
 
-<form action="https://preschool.dreamguystech.com/php-template/login.php">
-<div class="form-group">
-<input class="form-control" type="text" placeholder="Name">
+<form method="POST" >
+    <div class="form-group">
+<input class="form-control" type="text" name="name" placeholder="Name">
 </div>
 <div class="form-group">
-<input class="form-control" type="text" placeholder="Surname">
+<input class="form-control" type="text" name="surname" placeholder="Surname">
 </div>
 <div class="form-group">
-<input class="form-control" type="text" placeholder="Gender">
+<input class="form-control" type="text" name="gender" placeholder="Gender">
 </div>
 <div class="form-group">
-<input class="form-control" type="text" placeholder="Age">
+<input class="form-control" type="text" name="username" placeholder="Username">
 </div>
 <div class="form-group">
-<input class="form-control" type="text" placeholder="Username">
+<input class="form-control" type="password" name="password" placeholder="Password">
 </div>
 <div class="form-group">
-<input class="form-control" type="text" placeholder="Password">
+<input class="form-control" type="text" name="role" placeholder="Role">
 </div>
-<div class="form-group">
-<input class="form-control" type="text" placeholder="Role">
-</div>
-
 <div class="form-group mb-0">
-<button class="btn btn-primary btn-block" type="submit">Reset Password</button>
+<button class="btn btn-primary btn-block" name="register" type="submit">Register</button>
 </div>
 </form>
 
-<div class="text-center dont-have">Remember your password? <a href="login.php">Login</a></div>
+<div class="login-or">
+<span class="or-line"></span>
+<span class="span-or">or</span>
+</div>
+
+<div class="social-login">
+<span>Register with</span>
+<a href="#" class="facebook"><i class="fab fa-facebook-f"></i></a><a href="#" class="google"><i class="fab fa-google"></i></a>
+</div>
+
+<div class="text-center dont-have">Already have an account? <a href="login.php">Login</a></div>
 </div>
 </div>
 </div>
@@ -110,5 +115,5 @@ if(isset($_POST['register'])){
 <script src="assets/js/script.js"></script>
 </body>
 
-<!-- Mirrored from preschool.dreamguystech.com/php-template/forgot-password.php by HTTrack Website Copier/3.x [XR&CO'2014], Thu, 28 Oct 2021 11:11:58 GMT -->
+<!-- Mirrored from preschool.dreamguystech.com/php-template/register.php by HTTrack Website Copier/3.x [XR&CO'2014], Thu, 28 Oct 2021 11:11:58 GMT -->
 </html>
